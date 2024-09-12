9a9bbcad39c2d268aa4d12c17bba7bc02708dfe1
oselviajuliana <113408146+oselviajuliana@users.noreply.github.com>
2024-09-12 21:57:56 +0700

    Handson 2

--- soal1.java
+++ soal1.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class soal1 {
-    /** buatlah sebauah program yang mengambil 2 input number dari user
-     * program tersebut akan print out nilai yang paling  besar
-     */
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+    /*input angka pertama dari user*/
+    System.out.print("Masukan angka pertama:");
+    int num1 = scanner.nextInt();
+
+    /*input angka kedua dari user*/
+    System.out.print("Masukkan angka kedua:");
+    int num2 = scanner.nextInt();
+    // membandingkan kedua angka dan mencetak nilai yang lebih besar
+    if (num1 > num2){
+        System.out.println("Angka terbesar adalah: " + num1);
+    } else if(num2 > num1){
+        System.out.println("Angka terbesar adalah: " + num2);
+    } else{
+        System.out.println("Kedua angka sama besar.");
+    }
+        scanner.close();
+
+}
 }
